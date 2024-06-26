621a171cce69a03e4634122c68351068a9c0aa2c
Luis Diego <ldluisdie@gmail.com>
2024-06-25 22:10:07 -0600

    modificaciones al archivo script y pdf

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-function crearCuentaBancaria(saldoInicial) {
-    var saldo = saldoInicial;
-    function depositar (cantidad) {
+function crearCuentaBancaria (saldoInicial) {
+    let saldo = saldoInicial;
+    function depositar(cantidad) {
         if (cantidad > 0) {
             saldo += cantidad;
         } else {
-            console.log("La cantidad a depositar debe ser mayor a cero.");
+            console.log ("La cantidad depositada debe ser mayor a cero.");
         }
     }
+    //Método privado para reitirar dinero
     function retirar (cantidad) {
         if (cantidad > 0 && cantidad <= saldo) {
             saldo -= cantidad;
         } else {
-            console.log("La cantidad a retirar debe ser mayor a cero y no exceder el saldo disponible.")
+            console.log ("La cantidad a retirar debe ser mayor a cero y no exceder el saldo disponible.")
         }
     }
+    //Retornamos un objeto con métodos públicos
     return {
         consultarSaldo: function () {
             return saldo;
         },
-        realizaDeposito: function (cantidad) {
-            depositar(cantidad);
+        realizarDeposito: function (cantidad) {
+            depositar (cantidad);
         },
-        realizarRetiro: function(cantidad) {
-            retirar(cantidad);
+        realizarRetiro: function (cantidad) {
+            retirar (cantidad);
         }
-    };
+    }
 }
-var miCuenta = crearCuentaBancaria(1000);
+
+var miCuenta = crearCuentaBancaria (1000);
 console.log("Saldo inicial: " + miCuenta.consultarSaldo());
 miCuenta.realizarDeposito(500);
-console.log("Salo despues del deposito: " + miCuenta.consultarSaldo());
+console.log("Saldo después del depósito:" + miCuenta.consultarSaldo());
 miCuenta.realizarRetiro(200);
-console.log("Saldo despues del retiro: " + miCuenta.consultarSaldo());
+console.log("Saldo después del retiro: " + miCuenta.consultarSaldo());
 
-try {
-    miCuenta.depositar(100);
+//A continuación se pesenta un ejemplo de como manejar excepciones en JavaScript utilizando el bloque try... catch
+try { 
+    miCuenta.depositar(100); 
 } catch (e) {
     console.log(e.message);
-} 
+}
 try {
     miCuenta.retirar(100);
 } catch (e) {
-    console.log(e.message);
+    console.log(e.message)
 }
